7147c9e7855a23a885998a851f9ec92eadfa53b2
NoCluePS <pijus.serap@gmail.com>
2024-04-01 13:41:41 +0300

    added procentage

--- src/app/_components/Stats.tsx
+++ src/app/_components/Stats.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import { Decimal } from "decimal.js";
@@ -9,6 +9,20 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 const Stats = () => {
   const { data, isLoading } = api.stats.getAll.useQuery();
   const latestData = data?.[0];
+  const porcentageChange = useMemo(() => {
+    return latestData && data
+      ? new Decimal(latestData.retire)
+          .add(new Decimal(latestData.pijus))
+          .add(new Decimal(latestData.elze))
+          .times(100)
+          .dividedBy(
+            new Decimal(data[data.length - 1]!.retire ?? 0)
+              .add(new Decimal(data[data.length - 1]!.pijus ?? 0))
+              .add(new Decimal(data[data.length - 1]!.elze ?? 0)),
+          )
+          .minus(100)
+      : new Decimal(100);
+  }, [data, latestData]);
 
   if (isLoading) return <div>Kraunama...</div>;
 
@@ -36,18 +50,31 @@ const Stats = () => {
   };
 
   return (
-    <div className="flex gap-4 rounded-lg rounded-t-none p-4 shadow-md">
-      <div className="flex flex-1 flex-col items-center justify-center rounded-md p-3 text-xl shadow-lg">
-        Bendros santaupos
-        <span className="mt-[8px] text-3xl text-green-500">
-          {new Decimal(latestData.retire)
-            .add(new Decimal(latestData.pijus))
-            .add(new Decimal(latestData.elze))
-            .toString()}{" "}
-          €
-        </span>
+    <div className="rounded-lg rounded-t-none p-4 shadow-md">
+      <div className="flex gap-4">
+        <div className="flex flex-1 flex-col items-center justify-center rounded-md p-3 text-xl shadow-md">
+          Bendros santaupos
+          <span className="mt-[8px] text-3xl text-green-500">
+            {new Decimal(latestData.retire)
+              .add(new Decimal(latestData.pijus))
+              .add(new Decimal(latestData.elze))
+              .toString()}{" "}
+            €
+          </span>
+        </div>
+        <div className="flex flex-1 flex-col items-center justify-center rounded-md p-3 text-xl shadow-md">
+          <span>
+            Procentaliai (<span className="text-green-500">+</span>/
+            <span className="text-red-500">-</span>)
+          </span>
+          <span
+            className={`mt-[8px] text-3xl ${porcentageChange.greaterThan(0) ? "text-green-500" : "text-red-500"}`}
+          >
+            {porcentageChange.toFixed(0).toString()} %
+          </span>
+        </div>
       </div>
-      <div className="flex flex-grow flex-col items-center justify-center rounded-md p-3 text-xl shadow-lg">
+      <div className="flex flex-grow flex-col items-center justify-center rounded-md p-3 text-xl shadow-md">
         Išskaidytai
         <div className="my-3 max-w-[300px]">
           <Pie data={formattedData} />
